83bcc91d5b5a959c3e2c74a78e9976bc3e4210a0
FiniteZombie <edi.ramirez@gmail.com>
2017-08-09 02:56:55 -0700

    perspective cam!

--- Assets/ScaleTransformation.cs
+++ Assets/ScaleTransformation.cs
@@ -6,9 +6,9 @@ public class ScaleTransformation : Transformation {
     public override Matrix4x4 Matrix {
         get {
             var matrix = new Matrix4x4();
-            matrix.SetRow(0, new Vector4(Scale.x, 0f, 0f, 1f));
-            matrix.SetRow(1, new Vector4(0f, Scale.y, 0f, 1f));
-            matrix.SetRow(2, new Vector4(0f, 0f, Scale.z, 1f));
+            matrix.SetRow(0, new Vector4(Scale.x, 0f, 0f, 0f));
+            matrix.SetRow(1, new Vector4(0f, Scale.y, 0f, 0f));
+            matrix.SetRow(2, new Vector4(0f, 0f, Scale.z, 0f));
             matrix.SetRow(3, new Vector4(0f, 0f, 0f, 1f));
             return matrix;
         }
